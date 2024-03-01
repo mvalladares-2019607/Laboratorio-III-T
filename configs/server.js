8b8b8ac1c370b8370eb5e5a450984363928d7db4
mvalladares-2019607 <mvalladares-2019607@kinal.edu.gt>
2024-02-29 20:15:00 -0600

    se hizo el server.js

---
+++ configs/server.js
@@ -0,0 +1,43 @@
+'use strict' 
+
+import express from 'express'; 
+import cors from 'cros'; 
+import helmet from 'helmet'; 
+import morgan from 'morgan'; 
+import { dbConnection } from './mongo.js'; 
+import useRoutes from '../src/user/user.routes.js'; 
+import authRoutes from '../src/auth/auth.routes.js';
+import appoinmentRoutes from '../src/appointment/appointment.routes.js';
+
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT; 
+        
+        this.middlewares();  
+        this.conectarDB();  
+        this.routes();  
+    }
+    async conectarDB() {
+        await dbConnection();
+    }
+    middlewares() {
+        this.app.use(express.urlencoded({ extended: false }));
+        this.app.use(cors());
+        this.app.use(express.json());
+        this.app.use(helmet());
+        this.app.use(morgan('dev'));
+    }
+    routes() {
+        /*this.app.use(this.usuarioPath, userRoutes);
+        this.app.use(this.authPath, authRoutes);
+        this.app.use(this.appointmentPath, appoinmentRoutes);*/
+    }
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Server running on port ', this.port);
+        });
+    }
+}
+export default Server;
