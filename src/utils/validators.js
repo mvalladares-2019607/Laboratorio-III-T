b1512e4d33c01a061dc637dee81775195e7adbd9
mvalladares-2019607 <mvalladares-2019607@kinal.edu.gt>
2024-03-01 18:15:38 -0600

    se arreglo error

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,9 +1,6 @@
 'use strict'
 
 import {hash, compare} from 'bcrypt'
-
-
-//Encriptar la contraseña
 export const encrypt = (password)=>{
     try {
         return hash(password, 10)
@@ -12,8 +9,6 @@ export const encrypt = (password)=>{
         return error
     }
 }
-
-//Validar la contraseña
 export const checkPassword = async(password, hash)=>{
     try {
         return await compare(password, hash)
